caa15c1dab4323956bc281b4de99cffd957f70d2
Toru Maesaka <toru@torumk.com>
2023-03-29 08:45:01 -0500

    int: wrap standard NullInt64 functions

--- int.go
+++ int.go
@@ -2,6 +2,7 @@ package nullable
 
 import (
 	"database/sql"
+	"database/sql/driver"
 )
 
 // Int64 is a type alias against the standard sql.NullInt64 type.
@@ -10,6 +11,18 @@ type Int64 sql.NullInt64
 // NewInt64 returns an Int64 populated with the given int64.
 func NewInt64(val int64) Int64 {
 	return Int64{Int64: val, Valid: true}
+}
+
+// Scan wraps the standard Scan function, which implements the Scanner interface.
+func (i *Int64) Scan(value any) error {
+	ni := sql.NullInt64(*i)
+	return ni.Scan(value)
+}
+
+// Value wraps the standard Value function, which implements the driver Valuer interface.
+func (i Int64) Value() (driver.Value, error) {
+	ni := sql.NullInt64(i)
+	return ni.Value()
 }
 
 // Null returns true if the underlying value is NULL.
